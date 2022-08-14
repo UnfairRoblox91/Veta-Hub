8e4ceb4329667ae0d139692fbcca3a8f0b70dc5a
Fari <88715332+UnfairRoblox91@users.noreply.github.com>
2022-08-14 07:49:14 +0700

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,9 @@
+--[[
+    Supported games:
+    Tower of hell
+    Build a boat
+    Meep city
+    More coming soon..
+]]--
+
+loadstring(game:HttpGet("https://github.com/UnfairRoblox91/Veta-Hub/blob/main/Veta/Veta%20Hub.lua"), true)()
