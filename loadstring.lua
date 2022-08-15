2ba7d9eb94bb2724813bf98b91fa59e890460d9b
Fari <88715332+UnfairRoblox91@users.noreply.github.com>
2022-08-15 19:56:53 +0700

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -2,8 +2,13 @@
     Supported games:
     Tower of hell
     Build a boat
-    Meep city
+    Slap Battles
+    Clicker Simulator
     More coming soon..
+
+    Changelogs:
+    [-] Remove 1 games
+    [+] Add 2 games
 ]]--
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UnfairRoblox91/Veta-Hub/main/Veta/Veta%20Hub.lua", true))()
